4b2c6c86bd20e666120e15a3d3eac023fa4fef8e
Scott Lee <74874311+RobbinBob@users.noreply.github.com>
2025-03-23 19:22:28 +0000

    fixed incorrect file json attr

--- build/api-builder.py
+++ build/api-builder.py
@@ -25,11 +25,6 @@ pr_files_url = f"https://api.github.com/repos/{repo_full_name}/pulls/{pr_number}
 response = requests.get(pr_files_url, headers=headers)
 pr_files = response.json()
 
-print("Filed changed in this PR:")
-for file in pr_files:
-    print(f"- {file['filename']}")
-    print(file)
-
 # Load and update api.json
 api_json_path = "build/api.json"
 if os.path.exists(api_json_path):
@@ -46,7 +41,7 @@ for file in pr_files:
     print(f"- {file['filename']}")
     if 'nlvm' in file['filename']:
         print("NLVM file found")
-        decorator = ClassDecorator.ClassDecorator(file['filepath'])
+        decorator = ClassDecorator.ClassDecorator(file['filename'])
         class_json = decorator.decorate()
         print(f"Generated data {json.dump(class_json)}")
         api_data['classes'][class_json[ClassDecorator.JSON_TAG_TYPENAME]] = class_json
